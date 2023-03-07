20c85e62843ec429f6f06ac0d772bc1b866a4c05
alfeu23 <pedro.alfeu23@gmail.com>
2023-03-07 20:12:38 -0300

    Apresentacao Pedro Alfeu

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Pedro Alfeu, tenho 18 anos. Gosto de ler e jogar videogames. Não tenho experiência previa de programação, começei a programar em POO1. Espero nessa discplina aprofundar meus conhecimentos em python." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
