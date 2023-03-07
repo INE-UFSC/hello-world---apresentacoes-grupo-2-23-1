035e56d47ffa0b3f5c2c3cff860896afee8c27c0
Matheus Fernandes Bigolin <mfrdrbigolin@disroot.org>
2023-03-07 11:32:14 -0300

    Apresentação Matheus

--- membro1.py
+++ membro1.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Matheus, tenho 19 anos. Gosto de programar, de ler e de escrever. Já programo com algumas linguagens como C, JavaScript e Python (só checar meu perfil). Espero aprendar mais programação orientada a objetos nessa disciplina." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
