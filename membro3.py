d8e0c1a38a6c78bbc795153cf1bb9cdc16d7403c
Lucas Brand <lucbsm@gmail.com>
2023-03-07 21:11:48 -0300

    Apresentação Lucas

--- membro3.py
+++ membro3.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Lucas Brand, 18 anos, tenho como hobbies jogar videogame e xadrez, tenho pouca experiencia em programação, espero evoluir bastante meu nivel de programação básico e aprender mais tecnologias" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
